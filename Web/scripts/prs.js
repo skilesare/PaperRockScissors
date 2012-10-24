ad0978ab79a969cfc883ff341ee74abc68a1af44
unknown <afatheree@Austin-01.CourtHouseInternal.net>
2012-10-24 15:46:19 -0500

    Added ideas

--- Web/scripts/prs.js
+++ Web/scripts/prs.js
@@ -3,3 +3,8 @@ $(
 		$('body').text("No More Test");
 	}
 );
+
+//Ideas
+//simulate a random prs role between two players
+//evaluate the role.  Who won
+//keep score.  Best out of 5?  Best out of 7?
